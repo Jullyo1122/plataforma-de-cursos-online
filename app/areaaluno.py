88a92dcb1d92fd2281991613a5248bd07d3c1485
Jullyo1122 <jullyoc74@gmail.com>
2025-09-03 22:59:27 -0300

    desenvolvendo funções para area do aluno

--- app/areaaluno.py
+++ app/areaaluno.py
@@ -9,6 +9,28 @@ cursos = {
     "Front-end": "Fundamentos em Desenvolvimento Web"
 }
 
-@app.get("/")
+@app.get("/cursos")
 def catologo():
-    {"mensagem": "Cursos disponiveis", "cursos": cursos}
+    return {"mensagem": "Cursos disponiveis", "cursos": cursos}
+
+meus_cursos = {}
+
+@app.get("/meus-cursos")
+def acessar_cursos():
+    return {"mensagem": "Meus cursos", "acessar": meus_cursos}
+
+class Matricula(BaseModel):
+    curso_matricula: str
+
+@app.post("/matricula")
+def matricular(dados: Matricula):
+    curso = dados.curso_matricula
+
+    if curso not in cursos:
+        return {"status": "Erro", "mensagem": "Curso não encontrado no catálogo"}
+
+    if curso in meus_cursos:
+        return {"status": "Erro", "mensagem": "Você já está matriculado neste curso"}
+    
+    meus_cursos[curso] = cursos[curso]
+    return {"status": "Sucesso", "mensagem": f"Matrícula realizada no curso {curso}"}
